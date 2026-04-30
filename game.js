8fb04d3f5c09520eedc5ed34ba75f529dec2f85d
mcdanielle <dagdaganmcdaniell0807@gmail.com>
2026-04-30 22:33:58 +0800

    fixed game logic

--- game.js
+++ game.js
@@ -39,36 +39,20 @@ const tictactoe = (() => {
 
         for (let i = 0; i < 3; i++) {
             if (gameboard[x] === gameboard[x + 1] && gameboard[x + 1] === gameboard[x + 2] && gameboard[x] !== " ") {
-                gameboard = [
-                    " ", " ", " ",
-                    " ", " ", " ",  
-                    " ", " ", " "
-                ]
+                
                 return gameboard[x];
             } 
             
             if (gameboard[y] === gameboard[y + 3] && gameboard[y + 3] === gameboard[y + 6] && gameboard[y] !== " ") {
-                gameboard = [
-                    " ", " ", " ",
-                    " ", " ", " ",  
-                    " ", " ", " "
-                ]
+                
                 return gameboard[y];
             } 
 
             if (gameboard[z] === gameboard[z + 4] && gameboard[z + 4] === gameboard[z + 8] && gameboard[z] !== " ") {
-                gameboard = [
-                    " ", " ", " ",
-                    " ", " ", " ",  
-                    " ", " ", " "
-                ]
+                
                 return gameboard[z];
             } else if (gameboard[z + 2] === gameboard[z + 4] && gameboard[z + 4] === gameboard[z + 6] && gameboard[z + 2] !== " ") {
-                gameboard = [
-                    " ", " ", " ",
-                    " ", " ", " ",  
-                    " ", " ", " "
-                ]
+                
                 return gameboard[z + 2];
             }
 
@@ -114,11 +98,7 @@ buttons.forEach((button, index) => {
 
         button.textContent = tictactoe.getBoard()[index];
 
-        if(tictactoe.checkWinner() === "Draw") {
-            alert("Game Draw!");
-        } else if (tictactoe.checkWinner()) {
-            alert(`${tictactoe.checkWinner()} wins!`)
-        }
+        alertWinner();
 
         if (tictactoe.checkWinner()) {
             clearUI();
@@ -133,4 +113,13 @@ function clearUI() {
     buttons.forEach((element) => {
         element.textContent = "";
     })
+}
+
+function alertWinner() {
+    if (tictactoe.checkWinner() === "Draw") {
+        alert("Game Draw!")
+    } else if (tictactoe.checkWinner()) {
+        alert(`${tictactoe.checkWinner()} wins!`);
+    }
+
 }
